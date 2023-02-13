4fa6fa1d6d5470150f1c76655b5a8eb889d17832
rashidkhan <rashidkhanrk5456@email.com>
2023-02-13 19:01:37 +0530

    my class-3

---
+++ all type loop class-3/myscript.js
@@ -0,0 +1,67 @@
+//  loop
+
+
+// for loop
+
+// ans-1 1 se 100 tak print karao for loop se
+
+/*
+for(let a=1;a<=100;a++)
+{
+console.log(a)
+}
+*/
+
+
+// // ans-2 100 se 5 tak number print karao for loop se
+
+// for(let a=100;a>=5;a--){
+//     console.log(a)
+
+// }
+
+
+// ans-3 1 se 100 tak even number yadd krna
+
+
+
+// for(let a=1;a<=100;a++){
+// console.log(a++)
+// }
+
+
+// ans-3 1 se 100 tak even number yaad krna
+
+// for(let a=1;a<=100;a++)
+// if(a%2===0)
+// {
+// console.log("even")
+// }
+
+
+// ans-4 10 se 50 tak odd number yaad krna
+
+// for(let a=10;a<=50;a++)
+// if(a%2===1)
+// {
+//     console.log("odd")
+
+// }
+
+
+
+
+
+// while loop
+
+// ans-1 2 se 30 number tak print karao while loop se
+
+// let a=2;
+
+// while(a<=30){
+//     console.log(a);
+//     a++
+
+// }
+
+
