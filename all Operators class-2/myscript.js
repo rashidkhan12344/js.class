b7213b3ea1ea0e3b068a577277a4799f1eeb1d32
rashidkhan <rashidkhanrk5456@email.com>
2023-02-13 18:54:36 +0530

    your wecode

---
+++ all Operators class-2/myscript.js
@@ -0,0 +1,110 @@
+//  Operators
+
+// Arithmetic Operators
+
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Division Remainder)
+// ++	Increment
+// --	Decrement
+
+let a = 10;
+let b = 4;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a ** b);
+console.log(a / b);
+console.log(a % b);
+console.log(++a + b);
+console.log(--a - b);
+
+// Comparison Operators
+
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+// ?	ternary operator
+
+let c = 12;
+let d = 8;
+console.log(c == d);
+console.log(c === d);
+console.log(c!=d);
+console.log(c!==d);
+console.log(c>d);
+console.log(c<d);
+console.log(c>=d);
+console.log(c<=d);
+
+// type operator
+
+let name = "rashid";
+let mobile = 0434344;
+let male = true;
+let bignumber = 213444434n;
+let = [234];
+let array2 = ["wecode"];
+
+console.log(typeof name);
+console.log(typeof mobile);
+console.log(typeof male);
+console.log(typeof bignumber);
+console.log(typeof jjk);
+console.log(typeof array2);
+
+// logical operator
+
+// $$  logical AND
+
+// || logical  OR
+
+// ! logical not
+
+let y = 11;
+let z =16;
+
+// $$  logical AND
+
+if(y*2==22 && z*2==32){
+    console.log("two right then show ho");
+}
+
+// || logical  OR
+
+if(y-8==8 || z-10==6){
+    console.log("one right then show ho");
+}
+
+ 
+
+
+
+
+// ternary operator
+
+let s = 10;
+s <= 15 ? console.log("yes") : console.log("no");
+s >= 15 ? console.log("yes") : console.log("no");
+s * 2 == 20 ? console.log("yes") : console.log("no");
+s + 2 == 15 ? console.log("yes") : console.log("no");
+s - 5 == 5 ? console.log("yes") : console.log("no");
+s / 2 ? console.log("yes") : console.log("no");
+--s == 9 ? console.log("yes") : console.log("no");
+++s == 10 ? console.log("yes") : console.log("no");
+
+
+
+
+
+
+
