8e690527e3016ccacb75f3ba3b7a95d76d4baf79
rashidkhan <rashidkhanrk5456@email.com>
2023-02-13 18:51:07 +0530

    your name

---
+++ variable and Output type class-1/myscript.js
@@ -0,0 +1,50 @@
+// variable
+
+// var
+
+var mobile = 345545243;
+var email = "rashidkhan@email.com";
+
+// log
+
+console.log(mobile);
+
+console.log(email);
+
+console.log(mobile, email);
+
+// const
+
+const name = "rashid";
+const aadharnumber = 234453445;
+
+// log
+
+console.log(name);
+console.log(aadharnumber);
+
+console.log(name, aadharnumber);
+
+// error
+
+console.error(name);
+console.error(aadharnumber);
+
+// warn
+
+console.warn(name);
+console.warn(aadharnumber);
+
+// OutputType in four type
+
+// alert
+
+// alert("hello");
+
+// document.write
+
+document.write(5 + 10);
+
+// console
+
+console.log("rashidKhan");
