2def13d2f70af97303ba0f5514ba2b6e7057d34a
rashidkhan <rashidkhanrk5456@email.com>
2023-02-13 19:03:46 +0530

    class-4 and class-5

---
+++ switch case class-4/myscript.js
@@ -0,0 +1,123 @@
+// switch case
+
+// ans-1
+
+// let a=2;
+// switch(a){
+// case(1):
+// console.log(a);
+// case(2):
+// console.log(a);
+// case(3):
+// console.log(a);
+
+// }
+
+// // ans-2
+
+// let num="saturday";
+// switch(num){
+//     case("friday"):
+//     console.log(num);
+//     break;
+//     case("saturday"):
+//     console.log(num);
+//     break;
+//     case("sunday"):
+//     console.log(num);
+//     break;
+
+// }
+
+// // ans-3
+
+// let sum = "oct";
+// switch (sum) {
+//   case "jan":
+//   case "mar":
+//   case "may":
+//   case "jul":
+//   case "aug":
+//   case "oct":
+//   case "dec":
+//     console.log("31");
+//     break;
+//   case "apr":
+//   case "jun":
+//   case "sep":
+//   case "nov":
+//     console.log("30");
+//     break;
+//   case "feb":
+//     console.log("28");
+// }
+
+// // ans-4
+
+// let num="c";
+// switch(num){
+//     case("a"):
+//     case("b"):
+//     case("c"):
+//     case("d"):
+//     console.log("word");
+//     break;
+//     case("1"):
+//     case("2"):
+//     case("3"):
+//     case("4"):
+//     console.log("number");
+//     break;
+
+//     default:
+//         console.log("default enme se koi nhi");
+//         break;
+
+// }
+
+// ans-5
+
+// let num="4";
+// switch(num){
+//     case("a"):
+//     case("b"):
+//     case("c"):
+//     case("d"):
+//     console.log("word");
+//     break;
+//     case("1"):
+//     case("2"):
+//     case("3"):
+//     case("4"):
+//     console.log("number");
+//     break;
+
+//     default:
+//         console.log("default enme se koi nhi");
+//         break;
+
+// }
+
+// ans-6
+
+// let num="5";
+// switch(num){
+//     case("a"):
+//     case("b"):
+//     case("c"):
+//     case("d"):
+//     console.log("word");
+//     break;
+//     case("1"):
+//     case("2"):
+//     case("3"):
+//     case("4"):
+//     console.log("number");
+//     break;
+
+//     default:
+//         console.log("default enme se koi nhi");
+//         break;
+
+// }
+
